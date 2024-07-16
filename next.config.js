392246f311041cf3cafada0db5c511866b3a805a
Jo Sanchez <shvinjas@gmail.com>
2024-07-17 01:41:25 +1200

    v190

--- next.config.js
+++ next.config.js
@@ -11,13 +11,25 @@
 // export default nextConfig;
 
 // next.config.js
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: ["images.pexels.com"],
+//   },
+// };
+
+// module.exports = nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["images.pexels.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+    ],
   },
 };
 
 module.exports = nextConfig;
-
-
